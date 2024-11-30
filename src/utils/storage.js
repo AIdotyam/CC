7da96f602b2e458ceca29028e07918b5994cd0a2
Narendra Febriliansyah R <narendrabrilians@gmail.com>
2024-11-30 10:59:33 +0700

    fix: correct keyFilename in credentials setup

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,6 +1,6 @@
 const { Storage } = require("@google-cloud/storage");
 const storage = new Storage({
-  keyFilename: "../../credentials/storage.json",
+  keyFilename: "../../credentials/gcp-service-account.json",
 });
 
 const bucket = storage.bucket(process.env.BUCKET_NAME);
